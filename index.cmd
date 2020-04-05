4f7c8fb19ba16ceecfed3d0f1f7b8126b51e3141
yawnoc <leeconway@protonmail.com>
2020-04-06 00:24:32 +0800

    Add section CMD literals

--- index.cmd
+++ index.cmd
@@ -3,12 +3,19 @@
 %author Conway
 %title Conway's markdown
 %date-created 2020-04-05
-%date-modified 2020-04-05
+%date-modified 2020-04-06
 %resources
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="cmd.css">
 
 %%%%
+
+
+{% \{ \^ [\s]* % <code> %}
+{% [\s]* \^ \} % </code> %}
+
+{: {. : <span class="mandatory-argument">{ :}
+{: .} : }</span> :}
 
 
 # %title #
@@ -32,14 +39,63 @@ implemented in Python using regex replacements.
 
 ////
 
-Conway markdown (CMD) is the result of someone joking that
+Conway's markdown (CMD) is the result of someone joking that
 "the filenames would look like Windows executables from the 90s".
 Inspired by the backticks of John Gruber's [markdown][],
-Conway markdown uses fence-style constructs,
+Conway's markdown uses fence-style constructs,
 for which arbitrarily long runs of a delimiter symbol can be used
-to avoid needing to escape that symbol.
+to wrap shorter runs of that symbol.
 
 ////
+
+
+##literals
+  CMD literals
+##
+
+|| cmd-syntax
+  {^ (!! (! !!) {.content.} (!! !) !!) ^}
+||
+
+////
+
+Literal {^ {.content.} ^},
+with HTML syntax-character escaping and de-indentation.
+Whitespace around {^ {.content.} ^} is stripped.
+For {^ {.content.} ^} containing one or more consecutive exclamation marks
+followed by a closing round bracket,
+use a greater number of exclamation marks in the delimiters.
+
+////
+
+====
+* CMD
+  ````
+  (!!!!
+    Escaping: (! & < > !).
+    Whitespace stripping: {(!      yes      !)}.
+    Enough exclamation marks: (!!! (!! (! never !) !!) !!!).
+  !!!!)
+  ````
+
+* HTML
+  ````
+  (!!!!
+    Escaping: &amp; &lt; &gt;.
+    Whitespace stripping: {yes}.
+    Enough exclamation marks: (!! (! never !) !!).
+  !!!!)
+  ````
+
+* Rendered
+  ////
+    Escaping: (! & < > !).
+    Whitespace stripping: {(!      yes      !)}.
+    Enough exclamation marks: (!!! (!! (! never !) !!) !!!).
+  ////
+
+
+====
 
 
 %footer-element
