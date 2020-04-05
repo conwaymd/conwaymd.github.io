ab60002cad9bdaaf48185f613b7e0d8ce1cde31e
yawnoc <leeconway@protonmail.com>
2020-04-05 22:16:01 +0800

    Add CMD background to index

--- index.cmd
+++ index.cmd
@@ -1,10 +1,22 @@
 %%%%
 
+%author Conway
 %title Conway's markdown
+%date-created 2020-04-05
+%date-modified 2020-04-05
 
 %%%%
 
+
 # %title #
+
+
+||||
+  First created: %date-created \\
+  Last modified: %date-modified \\
+  Page source: [index.cmd][cmd-source]
+||||
+
 
 ////
 
@@ -12,3 +24,28 @@ Conway's fence-style markdown,
 implemented in Python using regex replacements.
 
 ////
+
+
+////
+
+Conway markdown (CMD) is the result of someone joking that
+"the filenames would look like Windows executables from the 90s".
+Inspired by the backticks of John Gruber's [markdown][],
+Conway markdown uses fence-style constructs,
+for which arbitrarily long runs of a delimiter symbol can be used
+to avoid needing to escape that symbol.
+
+////
+
+
+%footer-element
+
+
+@@[cmd-source]
+  https://github.com/conway-markdown/conway-markdown/blob/master/index.cmd
+@@
+
+@@[markdown]
+  https://daringfireball.net/projects/markdown/
+  Daring Fireball: Markdown
+@@
