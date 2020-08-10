35d16ede6b8f86cbde055380f675359eb65ab58e
yawnoc <yawnoc@pm.me>
2020-08-10 15:15:51 +0800

    Fix improper example for <i>
    Use <cite> rather than <i> for book titles.

--- index.cmd
+++ index.cmd
@@ -3,7 +3,7 @@
 %author Conway
 %title Conway's markdown (CMD)
 %date-created 2020-04-05
-%date-modified 2020-08-08
+%date-modified 2020-08-10
 %resources a~~
   <link rel="stylesheet" href="/cmd.min.css">
   <link rel="stylesheet"
@@ -2221,7 +2221,7 @@ The following delimiters {^ \X ^} are used:
   (bring to attention *without* strong importance,~e.g. keywords)
 
 * `_` for `<i>`
-  (offset text *without* stress emphasis, e.g.~book titles, Latin)
+  (offset text *without* stress emphasis, e.g.~Latin)
 
 ======
 
